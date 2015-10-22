9f5e2039d9d7a6f6b7272fda6aa3ea18064b3cdd
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-10-22 02:19:57 +0100

    bumps up receive rate to 115kbps

--- Python/TestSerial/rx.py
+++ Python/TestSerial/rx.py
@@ -1,12 +1,11 @@
 import serial
 import time
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1.0)
+port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=1.0)
 
 while True:
 	data = port.readline()
 	if data:
 		print ("\nreceived:\n"+data[:-1])
 	time.sleep(0.1)
-
 
