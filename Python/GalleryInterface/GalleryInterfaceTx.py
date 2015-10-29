91888d9a3d9ae3a6d6633b27498ae4fe30a02f33
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-10-29 16:35:12 +0100

    adds color and multiline messages to Gallery interface

--- Python/GalleryInterface/GalleryInterfaceTx.py
+++ Python/GalleryInterface/GalleryInterfaceTx.py
@@ -13,25 +13,20 @@ GREETING_TEXT = 'Please type some stuff / Escriba algo:'
 def drawScreen():
     global mScreen, mMessages
     for i,m in enumerate(mMessages):
-        s = ""
-        if m.startswith("**:"):
-            s = m.replace("**:", "")
-            mScreen.addstr(i,0,"**:", curses.color_pair(1))
-        elif m.startswith("~~:"):
-            s = m.replace("~~:", "")
-            mScreen.addstr(i,0,"~~:", curses.color_pair(2))
+        s = m[1:]
+        mScreen.addstr(i,0,"")
 
         s = "".join(s.split('\x00'))
-        mScreen.addstr(i,mScreen.getyx()[1],s)
+        mScreen.addstr(i,mScreen.getyx()[1],s, curses.color_pair(int(m[0])))
         mScreen.clrtoeol()
     mScreen.refresh()
-
 
 class writeThread (Thread):
     def __init__(self):
         Thread.__init__(self)
         self.mThreadRunning = False
         self.currentString = ""
+        self.cColor = 0
         mScreen.addstr(mScreen.getmaxyx()[0]-5,0,GREETING_TEXT+"\n"+self.currentString)
         mScreen.refresh()
     def run(self):
@@ -39,10 +34,13 @@ class writeThread (Thread):
         while(self.mThreadRunning):
             event = mScreen.getch()
             if event == ord("\n"):
-                if(len(mMessages) == mScreen.getmaxyx()[0]-6):
-                    del mMessages[0]
+                if(len(mMessages) >= mScreen.getmaxyx()[0]-7):
+                    del mMessages[0:3]
 
-                mMessages.append("**: "+self.currentString)
+                mMessages.append(str(self.cColor)+"DATE TIME:")
+                mMessages.append(str(self.cColor)+self.currentString)
+                mMessages.append(str(self.cColor)+"")
+                self.cColor = (self.cColor+1)%2
                 mSerial.write(self.currentString+'\n')
                 self.currentString  = ""
                 mScreen.addstr(mScreen.getmaxyx()[0]-5,0,GREETING_TEXT+"\n"+self.currentString)
@@ -76,8 +74,7 @@ def setup():
     curses.use_default_colors()
     curses.noecho()
     mScreen.keypad(1)
-    curses.init_pair(1, curses.COLOR_BLUE, -1)
-    curses.init_pair(2, curses.COLOR_RED, -1)
+    curses.init_pair(1, curses.COLOR_RED, -1)
 
     cprint("STARTING SERIAL PORT", 'green', attrs=['bold','reverse'], end='\n')
     mSerial = Serial(SERIAL_PORT_NAME, baudrate=SERIAL_BAUD_RATE, timeout=0.01, writeTimeout=0.5)
