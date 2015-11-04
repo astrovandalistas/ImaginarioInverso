690b7b6555cddea2009d55ca96bc00a1ece92c36
Thiago Hersan <thiago.hersan+github@gmail.com>
2015-11-03 21:19:34 -0700

    3s on 1.5s off

--- Arduino/LaserPwmPulse/LaserPwmPulse.ino
+++ Arduino/LaserPwmPulse/LaserPwmPulse.ino
@@ -1,17 +1,21 @@
 int LED_PIN = 9;
-int transDuration = 3000;
+int LASER_PIN = 13;
+int transDuration = 1500;
 
 void setup() {}
 
 void loop() {
-  if ((millis() / transDuration) % 3 == 0) {
+  if ((millis() / transDuration) % 3 != 0) {
     analogWrite(LED_PIN, 255);
+    analogWrite(LASER_PIN, 255);
     delay(random(32, 128));
     analogWrite(LED_PIN, 0);
+    analogWrite(LASER_PIN, 0);
     delay(random(32, 128));
   }
   else {
     analogWrite(LED_PIN, 0);
+    analogWrite(LASER_PIN, 0);
     delay(100);
   }
 }
